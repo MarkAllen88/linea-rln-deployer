36fab133dea206f4a9ae8b50dbdb670cbd1bbec0
MarkAllen88 <maui4marko@gmail.com>
2025-09-03 16:36:05 -0700

    Make persistent peer id (plus prev. rel.)

--- frontend/src/lib/wakuClient.ts
+++ frontend/src/lib/wakuClient.ts
@@ -5,7 +5,8 @@ import { multiaddr } from '@multiformats/multiaddr';
 // const localNodeMultiaddr = '/ip4/127.0.0.1/tcp/60000/ws';
 // const localNodeMultiaddr = '/dns4/localhost/tcp/60000/ws/p2p/16Uiu2HAkw8wAzCndKyvfHZj5JqLEU5i7VcUAgxpjzZUPVw5F2qiz';
 // const localNodeMultiaddr = '/ip4/127.0.0.1/tcp/60000/ws/p2p/16Uiu2HAkw8wAzCndKyvfHZj5JqLEU5i7VcUAgxpjzZUPVw5F2qiz';
-const localNodeMultiaddr = '/dns4/localhost/tcp/60000/ws/p2p/32ee547b3decdb2a326625d1af592c695181fc219cf8571bd3b1e029963ffb6b';
+// const localNodeMultiaddr = '/dns4/localhost/tcp/60000/ws/p2p/32ee547b3decdb2a326625d1af592c695181fc219cf8571bd3b1e029963ffb6b';
+const localNodeMultiaddr = import.meta.env.VITE_WAKU_NODE_MULTIADDR!;
 // const localNodeMultiaddr = '/ip4/127.0.0.1/tcp/60000/ws/32ee547b3decdb2a326625d1af592c695181fc219cf8571bd3b1e029963ffb6b';
 
 let wakuSingleton: any = null;
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-//
-// export default defineConfig({ plugins: [react(), tailwindcss()] })
+
+export default defineConfig({ plugins: [react(), tailwindcss()] })
 // If you want to be extra safe in dev, you can disable React Fast Refresh
-export default defineConfig({ plugins: [react({ fastRefresh: false }), tailwindcss()]})
+
+// export default defineConfig({ plugins: [react({ fastRefresh: false }), tailwindcss()]})
 
 
 
